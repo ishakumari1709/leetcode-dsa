bad0dcc6d68f82cbfc0251597e749ae6190ac847
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2024-11-16 22:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (13.30%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        auto dp1 = [](vector<int>& nums) {
+            int n = nums.size();
+            if (n == 0) return 0;
+            if (n == 1) return nums[0];
+            vector<int> dp(n, 0);
+            dp[0] = nums[0];
+            dp[1] = max(nums[0], nums[1]);
+            for (int i = 2; i < n; i++) {
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            }
+            return dp[n - 1];
+        };
+
+        vector<int> case1(nums.begin(), nums.end() - 1);
+        vector<int> case2(nums.begin() + 1, nums.end()); //beacuse its a circular array (we suppose )
+
+        return max(dp1(case1), dp1(case2)); 
+    }
+};
