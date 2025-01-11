924ba355a94a409e63eafe0ffccb68b51f4dc375
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-01-11 11:44:11 +0530

    Time: 38 ms (20.31%) | Memory: 154.7 MB (5.36%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -1,24 +1,32 @@
 class Solution {
+    private: bool isPossible(vector<int> batteries, long long mid,int n){
+        long long sum=0;
+        for(long long battery:batteries){
+            sum+=min(battery,mid);
+        }
+        return sum>=mid*n;
+    }
 public:
     long long maxRunTime(int n, vector<int>& batteries) {
-        long long s = 1, e = 0, mid = 0, sum = 0;
-        for (long long battery : batteries) {
-            sum += battery;
+        long long start=*min_element(batteries.begin(),batteries.end());
+        long long end=0;
+        for(long long battery: batteries){
+            end+=battery;
         }
-        e = sum /n;
-        while (s <= e) {
-            mid = (s + e) / 2;
-            long long total_time = 0;
-            for (long long battery : batteries) {
-                total_time += min(battery, mid);
+
+        long long mid;
+        while(start<=end){
+            mid=(start+end)/2;
+            if(isPossible(batteries,mid,n)){
+                start=mid+1;
             }
-            if (total_time >= mid * n) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
+            else{
+                end=mid-1;
             }
+
+            
         }
-        return e;
+       return end; 
+        
     }
 };
-
