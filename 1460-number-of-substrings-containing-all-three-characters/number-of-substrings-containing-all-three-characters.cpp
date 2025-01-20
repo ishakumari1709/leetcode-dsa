4c4e9cdb9e38994f9cb04c20764d8f47343ba139
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-01-20 22:31:51 +0530

    Time: 23 ms (32.65%) | Memory: 11.4 MB (20.08%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        int count=0;
         int start=0;
         int end=0;
-        int count=0;
         unordered_map<char,int> map;
         while(end<s.length()){
             map[s[end]]++;
-            while(map['a']>0 && map['b']>0 && map['c']>0){ // Check if the window contains at least one 'a', 'b', and 'c'
+            while(map['a'] >0 && map['b'] && map['c'] >0){
                 count+=s.length()-end;
-                map[s[start]]--; //start shrinking from start
+                map[s[start]]--;
                 start++;
             }
             end++;
         }
-        
         return count;
     }
 };
