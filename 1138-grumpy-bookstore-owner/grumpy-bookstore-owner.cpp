5c7cbdf209fed998628e10379d342d41cb6bef40
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-01-20 17:00:25 +0530

    Time: 3 ms (28.74%) | Memory: 35.7 MB (89.24%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int unsatisfiedCustomers=0;
-        for(int i=0;i<minutes;i++){
-            unsatisfiedCustomers+=grumpy[i]*customers[i]; //multiplied because (multiplied by 0 eill result in 0 and by 1 will result in same number)
+        int strat=0;
+        int satisfied=0;
+        int additionalSatisfied=0;
+        int  maxAdditionalSatisfied=0;
+        int n=customers.size();
+        for(int i=0;i<n;i++){
+            if(grumpy[i]==0){
+                satisfied+=customers[i];
+            }
         }
-        int maxUnsatisfiedCustomers=unsatisfiedCustomers;
-        for(int i=minutes;i<customers.size();i++){
-            unsatisfiedCustomers+=grumpy[i]*customers[i];
-            unsatisfiedCustomers-=grumpy[i-minutes]*customers[i-minutes];
-            maxUnsatisfiedCustomers=max(maxUnsatisfiedCustomers,unsatisfiedCustomers);
-            
+        for(int i=0;i<n;i++){
+            if(grumpy[i]==1){
+               additionalSatisfied+=customers[i];
+            }
+            if(i>=minutes && grumpy[i-minutes]==1){
+                additionalSatisfied-=customers[i-minutes];
+            }
+            maxAdditionalSatisfied = max(maxAdditionalSatisfied, additionalSatisfied);
         }
-        int maxSatisfiedCustomers=maxUnsatisfiedCustomers;
-        for(int i=0;i<customers.size();i++){
-            maxSatisfiedCustomers+=(1-grumpy[i])*customers[i];
-        }
-        return maxSatisfiedCustomers;
+
+        return satisfied + maxAdditionalSatisfied;
+        
         
     }
 };
