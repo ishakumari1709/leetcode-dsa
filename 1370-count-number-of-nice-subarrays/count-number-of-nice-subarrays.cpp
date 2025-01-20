b79c41ba7038b8f829aa6d45edbe2f9864c71ece
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-01-20 16:33:39 +0530

    Time: 11 ms (44.91%) | Memory: 78 MB (25.54%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,24 +1,28 @@
 class Solution {
-public:
-    int countSubarraysWithAtMostKodds(vector<int>& nums,int k){
-        int start=0,answer=0,count=0;//count=no of odds
-        for(int end=0;end<nums.size();++end){
-            if(nums[end]%2!=0){ //if the current no  is odd
-                count++; //increment the count od odd numbers
+    private: int helper(vector<int> nums,int k){
+        int start=0;
+        int end=0;
+        int count=0;
+        int totalS=0;
+       for(end=0;end<nums.size();end++){
+            if(nums[end]%2!=0){
+                count++;
+
             }
             while(count>k){
-                if(nums[start]%2 !=0){
-                    count--;
-                }
-                start++;
+               if(nums[start]%2 !=0){
+                count--;
+                
+               }
+               start++;
+              
             }
-            answer+=end-start+1;
+            totalS+=end-start+1;
         }
-        return answer;
+        return totalS;
     }
+public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return countSubarraysWithAtMostKodds(nums,k)-countSubarraysWithAtMostKodds(nums,k-1);
-
-        
+        return helper(nums,k)-helper(nums,k-1);
     }
 };
