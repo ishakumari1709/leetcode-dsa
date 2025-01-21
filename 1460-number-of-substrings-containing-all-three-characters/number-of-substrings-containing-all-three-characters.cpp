f7c506f39e19d54b785f57b64ef2c96a4ba5f335
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-01-21 11:12:01 +0530

    Time: 25 ms (31.42%) | Memory: 11.3 MB (24.95%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -7,13 +7,12 @@ public:
         unordered_map<char, int> map;
         while (end < s.length()) {
             map[s[end]]++;
-            while (map['a'] > 0 && map['b'] > 0 && map['c'] > 0) {
+            while(map['a'] > 0 && map['b'] > 0 && map['c'] > 0) {
 
                 map[s[start]]--;
                 start++;
                 totalS += s.size() - end;
             }
-
             end++;
         }
         return totalS;
