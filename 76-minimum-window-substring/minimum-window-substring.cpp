d5fc10bd462d3c5bc2788ea459b666c1760d77a2
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-01-22 13:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (40.04%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,48 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        
-        int frequencyT[128]={0};
-        int frequencyS[128]={0};
-        int lengthS = 0;
-        int lengthT = 0;
-        int start=0,end=0,l=0;
-        int start_i=-1,end_i=-1;
-        int minLength=INT_MAX;
-        if(t.length()>s.length()){
-            return "";
-        }
-         for(int i=0;i<t.size();i++){
-            char c= t[i];
-            ++frequencyT[c]; //this increase the freqeuncy of each character 
-
-            if (frequencyT[c]== 1) {  
-                ++lengthT;// and this increase the length of t 
+        int start=0;
+        int end=0;
+        int unique=0;
+        int startIndex=-1;
+        int endIndex=-1;
+        int freqT[128]={0};
+        int freqS[128]={0};
+        int minLength=s.length()+1;
+        for(char c: t){
+            if(freqT[c]==0){
+                ++unique;
             }
-        
-
+            freqT[c]++;
         }
         while(end<s.length()){
             char c=s[end];
-            ++frequencyS[c];
-            if(frequencyS[c]==frequencyT[c]){
-                ++lengthS;
+            ++freqS[c];
+            if(freqS[c]==freqT[c]){
+                --unique;
             }
-            while(lengthS==lengthT){
-                if(end - start+1 < minLength){
-                    start_i=start;
-                    end_i=end;
+            while(unique==0){
+                if(end-start+1<minLength){
+                    startIndex=start;
                     minLength=end-start+1;
                 }
-                char ch=s[start]; //to remove the chadacter
-                if(frequencyS[ch]==frequencyT[ch]){
-                    --lengthS;
+                char ch=s[start];
+                if(freqS[ch]==freqT[ch]){
+                    ++unique;
                 }
-                --frequencyS[ch];
                 ++start;
+                --freqS[ch];
             }
-            ++end;
-        }
-       return start_i == -1 ? "" : s.substr(start_i, minLength);   
-    }
+           ++end; 
+           
+    }
+    return startIndex==-1 ? "": s.substr(startIndex,minLength);
+    }
 };
