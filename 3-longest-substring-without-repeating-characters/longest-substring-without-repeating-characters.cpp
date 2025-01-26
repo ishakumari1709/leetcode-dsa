66fe07299d8141362715fa1de14fe348911832b2
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-01-26 18:03:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (91.40%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,20 +3,18 @@ public:
     int lengthOfLongestSubstring(string s) {
         int start=0;
         int maxL=0;
-        vector<int> freqIdx(128, -1);
-        for(int end=0;end<s.length();end++){
-            char currChar=s[end];
-            if(freqIdx[currChar]>=start){
-                start=max(start,freqIdx[currChar]+1);
+        vector<int> frequency(128,0);
+        for(int end=0;end<s.size();end++){
+            char currentChar=s[end];
+            frequency[currentChar]++;
+            while(frequency[currentChar]>1){
+                frequency[s[start]]--;
+                start++;
             }
-            freqIdx[currChar]=end;
             maxL=max(maxL,end-start+1);
+            
         }
         return maxL;
-
-        }
-            
-
-            
         
+    }
 };
