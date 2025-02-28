6b4d08b1bd315a968c9f3ba2e4ab88ea28e3c353
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-03-01 00:31:30 +0530

    Time: 4 ms (56.80%) | Memory: 17 MB (39.54%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -3,14 +3,14 @@ public:
     string largestNumber(vector<int>& nums) {
         vector<string> str;
         for(int i=0;i<nums.size();i++){
-          str.push_back(to_string(nums[i]));
+          str.push_back(to_string(nums[i])); //["3", "30", "34", "5", "9"]
         }
         sort(str.begin(), str.end(), [](string &a, string &b) {
-        return a + b > b + a;
+        return a + b > b + a; //"95" > "59"
         });
         if(str[0]=="0") return "0";
         string result;
-        for(int i=0;i<str.size();i++){
+        for(int i=0;i<str.size();i++){ //start pushing in new vector
             result+=str[i];
         }
         return result;
