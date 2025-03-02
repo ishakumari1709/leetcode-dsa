178395b499f8b80b8f0d80fc71db0723d4072d1d
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-03-02 18:13:03 +0530

    Time: 3 ms (15.42%) | Memory: 9.1 MB (44.05%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     bool areSentencesSimilar(string sentence1, string sentence2) {
-        if(sentence1==sentence2) return true;
-        vector<string> words1,words2;
-        stringstream ss1(sentence1) ,ss2(sentence2);
+        if (sentence1 == sentence2) return true;
+
+        vector<string> words1, words2;
+        stringstream ss1(sentence1), ss2(sentence2);
         string word;
-       
-        while(ss1>>word) words1.push_back(word);
-        while(ss2>>word) words2.push_back(word);
-        int m=words1.size();
-        int n=words2.size();
-        int i=0;
-        int j=0;
-        while(i<m && i<n && words1[i]==words2[i]) i++;
-        while(j<m-i && j<n-i && words1[m-1-j]==words2[n-1-j]) j++;
-        return i+j==min(m,n);
-        
-    }
+        while (ss1 >> word) words1.push_back(word);
+        while (ss2 >> word) words2.push_back(word);
+        if (words1.size() > words2.size()) swap(words1, words2);
+        int m = words1.size(), n = words2.size();
+        int i = 0, j = 0;
+        while (i < m && words1[i] == words2[i]) i++;
+        while (j < m - i && words1[m - 1 - j] == words2[n - 1 - j]) j++;
+        return (i + j == m);
+       }
 };
