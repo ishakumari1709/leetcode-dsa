31f8c4d91e0bf7edef014eed6d5247d27f84d874
ishakumari1709 <144309840+ishakumari1709@users.noreply.github.com>
2025-03-03 14:39:09 +0530

    Time: 34 ms (30.28%) | Memory: 14.5 MB (35.62%) - LeetSync

---
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkIfCanBreak(string s1, string s2) {
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        bool s1flag =true;
+        bool s2flag=true;
+        if(s1.size()!=s2.size()) return false;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]<s2[i]){
+                 s1flag=false;
+            }
+            if(s2[i]<s1[i]){
+               s2flag =false;  
+            }
+        }
+        
+            
+
+        
+
+
+                
+
+        
+        if(s1flag==true || s2flag==true) {
+            return true;
+        } else{
+        return false;
+        }
+        
+    }
+};
