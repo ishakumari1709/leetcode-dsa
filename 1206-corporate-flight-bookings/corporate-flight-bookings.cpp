282c0c7ab3e08a81543eb7e010ca616385879e30
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-03-17 22:10:05 +0530

    Time: 2012 ms (12.56%) | Memory: 72 MB (98.33%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-         vector<int> prefixSum(n, 0);
-        for (int i = 0; i < bookings.size(); ++i) {
-            int start = bookings[i][0] - 1; 
-            int end = bookings[i][1];      
-            int seats = bookings[i][2];   
-
-            prefixSum[start] += seats;
-            if (end < n) {
-                prefixSum[end] -= seats;
+        vector<int> result(n, 0);
+        for (const auto& booking : bookings) {
+            int first = booking[0] - 1; 
+            int end = booking[1] - 1;
+            int seats = booking[2];
+            for (int i = first; i <= end; i++) {
+                result[i] += seats;
             }
         }
-        for (int i = 1; i < n; ++i) {
-            prefixSum[i] += prefixSum[i - 1];
-        }
-
-        return prefixSum;
-    }
+        return result;
 
         
-    
+    }
 };
