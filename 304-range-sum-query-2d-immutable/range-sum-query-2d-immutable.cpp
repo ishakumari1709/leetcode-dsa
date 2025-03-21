d5937b8f8fc7bc78a2b27afe68ca03506e06e946
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-03-21 12:38:09 +0530

    Time: 14 ms (90.17%) | Memory: 149.1 MB (84.46%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -1,33 +1,32 @@
 class NumMatrix {
+    vector<vector<int>> prefix;
 public:
-   vector<vector<int>> prefixSum;
     NumMatrix(vector<vector<int>>& matrix) {
-      int n = matrix.size();
-        int m = matrix[0].size();
-
-       
-        prefixSum = vector<vector<int>>(n, vector<int>(m, 0));
-
-        
-        prefixSum[0][0] = matrix[0][0];
-        for (int i = 1; i < m; i++) {
-            prefixSum[0][i] = prefixSum[0][i - 1] + matrix[0][i];
+        int row=matrix.size();
+        int col=matrix[0].size();
+        prefix=vector<vector<int>>(row,vector<int> (col,0));
+        prefix[0][0]=matrix[0][0];
+        for(int i=1;i<col;i++){
+            prefix[0][i]=prefix[0][i-1]+matrix[0][i];
         }
-        for (int j = 1; j < n; j++) {
-            prefixSum[j][0] = prefixSum[j - 1][0] + matrix[j][0];
+        for(int i=1;i<row;i++){
+            prefix[i][0]=prefix[i-1][0]+matrix[i][0];
         }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                prefixSum[i][j] = matrix[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+        for(int i=1;i<row;i++){
+            for(int j=1;j<col;j++){
+                prefix[i][j]=matrix[i][j]+prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
             }
         }
+
+        
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return prefixSum[row2][col2] 
-       - (row1 > 0 ? prefixSum[row1 - 1][col2] : 0) 
-       - (col1 > 0 ? prefixSum[row2][col1 - 1] : 0) 
-       + (row1 > 0 && col1 > 0 ? prefixSum[row1 - 1][col1 - 1] : 0);
+        int total = prefix[row2][col2];
+        int top = (row1 > 0) ? prefix[row1-1][col2] : 0;
+        int left = (col1 > 0) ? prefix[row2][col1-1] : 0;
+        int topLeft = (row1 > 0 && col1 > 0) ? prefix[row1-1][col1-1] : 0;
+        return total - top - left + topLeft;
         
     }
 };
