2cc94986c9f55ec1f7566a85f4c60c22251e51ad
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-04-11 23:39:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.00%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;  
-    unordered_map<char, char> matchingBrackets = {
-        {')', '('}, 
-        {'}', '{'}, 
-        {']', '['}
-    };
-
-    for (char ch : s) {
-        if (matchingBrackets.count(ch)) {
-            if (!st.empty() && st.top() == matchingBrackets[ch]) {
-                st.pop();  
-            } else {
-                return false;  
-            }
-        } else {
-            st.push(ch);  
+        string st;
+        map<char,char>mp;
+        mp['('] = ')';
+        mp['{'] = '}';
+        mp['['] = ']';
+        st.push_back(s[0]);
+        for (int i = 1; i < s.size(); i++) {
+            if(!st.empty() && mp[st.back()] == s[i])
+                st.pop_back();
+            else
+                st.push_back(s[i]);
         }
-    }
-    return st.empty();
+        return st.size() ? 0 : 1;
+
+        
     }
 };
