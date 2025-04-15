b932d772e5ec8c7647049152dab023377ecd321b
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-04-15 21:36:32 +0530

    Time: 2 ms (66.73%) | Memory: 132 MB (14.60%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,36 +9,19 @@
  * };
  */
 class Solution {
-    ListNode* reverseLL(ListNode* head){
-        ListNode* curr=head;
-        ListNode* next=nullptr;
-        ListNode* prev=nullptr;
-        while(curr!=nullptr){
-           next=curr->next;
-           curr->next=prev;
-           prev=curr;
-           curr=next;
-        }
-        return prev;
-    }
 public:
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
+        vector<int> listVals;
+        while(head!=nullptr){
+            listVals.push_back(head->val);
+            head=head->next;
         }
-        ListNode* secondHalf = reverseLL(slow);
-        ListNode* copySecond = secondHalf; 
-        ListNode* firstHalf = head;
-        while(secondHalf){
-            if(firstHalf->val != secondHalf->val) return false;
-            firstHalf = firstHalf->next;
-            secondHalf = secondHalf->next;
+        int left=0,right=listVals.size()-1;
+        while(left<right && listVals[left]==listVals[right]){
+            left++;
+            right--;
         }
-
-        return true;
+        return left>=right;
+        
     }
 };
