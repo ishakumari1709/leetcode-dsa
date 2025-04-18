93f5d358c4a7fc180d2a4c24095a19e16f8d4f28
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-04-18 19:46:13 +0530

    Time: 46 ms (17.36%) | Memory: 92.1 MB (27.97%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -6,16 +6,29 @@ stack<pair<int,int>>st;
     }
     
     int next(int price) {
-        int span=1;
-        while(!st.empty() && price>=st.top().first){
-            span+=st.top().second;
+        int count=1;
+        while(!st.empty() && st.top().first <=price){
+            count+=st.top().second;
             st.pop();
         }
-        st.push({price,span});
-        return span;
+        st.push({price,count});
+        return count;
         
     }
 };
+/*Initialize an empty stack to store (price, span).
+
+For each new price:
+
+Start with span = 1 (today counts).
+
+While the stack is not empty and top price ≤ current price:
+
+Pop the top and add its span to the current span.
+
+Push the current (price, span) to the stack.
+
+Return the span.*/
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
