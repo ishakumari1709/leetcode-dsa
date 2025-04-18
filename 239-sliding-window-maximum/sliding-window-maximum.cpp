3e79e8282c6a80198b627c23a11bdfa16fe580d7
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-04-18 15:13:37 +0530

    Time: 22 ms (67.18%) | Memory: 139.1 MB (83.85%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
-        if (n == 0) return {};
-
-        vector<int> left_max(n), right_max(n), result(n - k + 1);
-        for (int i = 0; i < n; ++i) {
-            if (i % k == 0) {
-                left_max[i] = nums[i]; 
-            } else {
-                left_max[i] = max(left_max[i - 1], nums[i]);
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        deque<int> dq;
+        for(int i=0;i<nums.size();i++){
+            int num=nums[i];
+            while(!dq.empty() && dq.back()<num){
+                dq.pop_back();
+            }
+            dq.push_back(num);
+            if(i>=k && nums[i-k]==dq.front()){
+                dq.pop_front();
+            }
+            if(i>=k-1){
+                res.push_back(dq.front());
             }
         }
-        for (int i = n - 1; i >= 0; --i) {
-            if (i == n - 1 || (i + 1) % k == 0) {
-                right_max[i] = nums[i];
-            } else {
-                right_max[i] = max(right_max[i + 1], nums[i]);
-            }
-        }
-        for (int i = 0; i <= n - k; ++i) {
-            result[i] = max(right_max[i], left_max[i + k - 1]);
-        }
-
-        return result;   
+        return res;
         
     }
 };
