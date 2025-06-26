cdaae24e09e493a116004823b9394e3b0b1d61a2
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-06-26 16:22:40 +0530

    Time: 32 ms (29.27%) | Memory: 23.8 MB (25.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,30 +1,50 @@
+
 class Solution {
-private:
-    void dfs(vector<vector<char>>& grid,int row,int col,int rows,int cols){
-        if(row<0||col<0||row==rows||col==cols||grid[row][col]=='0'){
-        return;
-        }
-        grid[row][col]='0';
-        dfs(grid,row-1,col,rows,cols);
-        dfs(grid,row,col-1,rows,cols);
-        dfs(grid,row+1,col,rows,cols);
-        dfs(grid ,row,col+1,rows,cols);
-
-    }    
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int answer=0;
-        int rows=grid.size();
-        int cols=rows==0?0:grid[0].size();
-        for(int row=0;row<rows;row++){
-            for(int col=0;col<cols;col++){
-                if(grid[row][col]=='1'){
-                    ++answer;
-                    dfs(grid,row,col,rows,cols);
-                }
+      int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(); //row
+        int n = grid[0].size(); //col
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        int count = 0; //no of island
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    bfs(i, j, grid, visited, m, n);
+                    count++;
+                }
             }
         }
-      return answer;  
+        return count;
+    }
+
+private:
+    void bfs(int row, int col,
+             vector<vector<char>>& grid,
+             vector<vector<bool>>& visited,
+             int rowL, int colL) {
+
+        visited[row][col] = true;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+
+        int d_r[] = {1, 0, 0, -1}; // Down, Left, Right, Up
+        int d_c[] = {0, -1, 1, 0};
+
+        while (!q.empty()) {
+            auto [r, c] = q.front(); q.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int nRow = r + d_r[k];
+                int nCol = c + d_c[k];
+
+                if (nRow >= 0 && nRow < rowL && nCol >= 0 && nCol < colL &&
+                    grid[nRow][nCol] == '1' && !visited[nRow][nCol]) {
+                    
+                    visited[nRow][nCol] = true;
+                    q.push({nRow, nCol});
+                }
+            }
+        }
     }
 };
