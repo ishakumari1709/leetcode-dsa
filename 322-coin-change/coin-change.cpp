85f6225a3976f49c2bb6573e5f674b8459386a63
Isha Kumari <144309840+ishakumari1709@users.noreply.github.com>
2025-08-14 23:11:14 +0530

    Time: 18 ms (85.74%) | Memory: 17.9 MB (85.20%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-         int n = coins.size();
-        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, INT_MAX));
+        vector<int> minCoins(amount + 1, amount + 1);
+        minCoins[0] = 0;
 
-        
-        for (int i = 0; i <= n; ++i) {
-            dp[i][0] = 0;
-        }
-
-        
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= amount; ++j) {
-                if (coins[i - 1] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    if (dp[i][j - coins[i - 1]] != INT_MAX) {
-                        dp[i][j] = min(dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
-                    } else {
-                        dp[i][j] = dp[i - 1][j];
-                    }
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (i - coins[j] >= 0) {
+                    minCoins[i] = min(minCoins[i], 1 + minCoins[i - coins[j]]);
                 }
             }
         }
-        return dp[n][amount] == INT_MAX ? -1 : dp[n][amount];
+
+        return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;
+        
     }
 };
